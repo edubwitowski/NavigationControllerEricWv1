1798a931040bb149455049cdd732367881b1b464
edubwitowski <31215451+edubwitowski@users.noreply.github.com>
2018-03-05 17:37:37 -0600

    Initial Commit

---
+++ NavigatorControllerEricWv1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigatorControllerEricWv1
+//
+//  Created by Macbook on 3/5/18.
+//  Copyright © 2018 Eric Witowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
